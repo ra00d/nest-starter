742e9ebad8f6e26e32b284ed9e843ed463781cd0
ra00d <84684356+ra00d@users.noreply.github.com>
2024-07-14 01:55:31 +0300

    add env support for data-source

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,26 +1,30 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Global()
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => {
-        return {
+        const options: TypeOrmModuleOptions = {
           type: 'mysql',
           host: configService.getOrThrow('DATABASE_HOST'),
           username: configService.getOrThrow('DATABASE_USER'),
           password: configService.getOrThrow('DATABASE_PASSWORD'),
           database: configService.getOrThrow('DATABASE_NAME'),
           port: configService.getOrThrow('DATABASE_PORT'),
-          synchronize: false, // configService.getOrThrow('NODE_ENV') === 'development',
-          logger: 'debug',
-          logging: ['query', 'error'],
+          synchronize: configService.getOrThrow('DATABASE_SYNCHRONIZE'), // configService.getOrThrow('NODE_ENV') === 'development',
+          // logger: 'debug',
+          // logging: ['query', 'error'],
+          logging: true,
           entities: [__dirname + '/../**/*.entity{.ts,.js}'],
           autoLoadEntities: true,
         };
+        console.log(options);
+
+        return options;
       },
       inject: [ConfigService],
     }),
