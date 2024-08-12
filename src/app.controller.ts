00ba96230f46eea412492f3ff31d93eec126d9df
ra00d <84684356+ra00d@users.noreply.github.com>
2024-08-13 01:50:33 +0300

    fix roles gaurd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,16 @@
-import { Controller, Get, Render, Req, Res, UseGuards } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { Setting } from 'main/settings/entities/setting.entity';
 import { I18n, I18nContext } from 'nestjs-i18n';
 import { EntityManager } from 'typeorm';
 import { Public } from './auth/decorators/public.decorator';
-import { SessionGuard } from './auth/guards/session.guard';
 import { navItems } from './constants/frontend/nav-items.constants';
-import { TestmailService } from 'mailer/services/testMail.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly db: EntityManager,
-    private mailer: TestmailService,
+    // private mailer: TestmailService,
   ) {}
 
   @Get()
@@ -24,7 +22,7 @@ export class AppController {
     const lang = i18n.lang;
     return {
       title: 'Home',
-      name: 'Tahfeedh',
+      name: 'Site name',
       lang: i18n.lang === 'ar' ? 'en' : 'ar',
       message: 'Hello world daisy',
       navItems: navItems,
@@ -46,23 +44,12 @@ export class AppController {
     res.cookie('lang', 'ar');
     res.redirect('/');
   }
-
-  @Get('test')
-  @UseGuards(SessionGuard)
-  async getAbout(@I18n() i18n: I18nContext, @Req() req: Request) {
-    return {
-      title: 'About',
-      name: 'Tahfeedh',
-      lang: i18n.lang === 'ar' ? 'en' : 'ar',
-      navItems: navItems,
-      user: req.user,
-    };
-  }
-  @Get('mail')
-  async sendMail() {
-    await this.mailer.send();
-    return {
-      msg: 'mail sent',
-    };
-  }
+  // NOTE: test email
+  // @Post('mail')
+  // async sendMail() {
+  //   await this.mailer.send();
+  //   return {
+  //     msg: 'mail sent',
+  //   };
+  // }
 }
